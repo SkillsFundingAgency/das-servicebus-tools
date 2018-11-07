091037a4a0b313a060833dbb417f0fff938d3fd9
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-11-07 09:33:07 +0000

    Updated to support command line queries

---
+++ src/SFA.DAS.NServiceBus.Tools.MessagePublisher/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace SFA.DAS.NServiceBus.Tools.MessagePublisher
+{
+    public static class StringExtensions
+    {
+        public static string HtmlDecode(this string input)
+        {
+            return WebUtility.HtmlDecode(input);
+        }
+    }
+}
