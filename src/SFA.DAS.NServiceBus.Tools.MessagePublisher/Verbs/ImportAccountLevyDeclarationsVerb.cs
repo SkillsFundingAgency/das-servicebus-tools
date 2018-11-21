960d3695cefc25e2835c2cb4c6e5f6faec28924c
Peter Williams <peter.david.williams.1991@gmail.com>
2018-11-21 15:17:16 +0000

    changed payments to levy per Robin's comment

--- src/SFA.DAS.NServiceBus.Tools.MessagePublisher/Verbs/ImportAccountLevyDeclarationsVerb.cs
+++ src/SFA.DAS.NServiceBus.Tools.MessagePublisher/Verbs/ImportAccountLevyDeclarationsVerb.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.NServiceBus.Tools.MessagePublisher.Verbs
     [Verb("importdeclarations", HelpText = "Import levy declarations for a PAYE scheme into an account.")]
     public class ImportAccountLevyDeclarationsVerb : NServiceBusVerbBase
     {
-        [Option('a', "account", HelpText = "The account ID you wish to import payments to.")] public long AccountId { get; set; }
+        [Option('a', "account", HelpText = "The account ID you wish to import levy to.")] public long AccountId { get; set; }
         [Option('p', "payescheme", HelpText = "The PAYE scheme you wish to import payment for.")] public string PayeRef { get; set; }
     }
 }
