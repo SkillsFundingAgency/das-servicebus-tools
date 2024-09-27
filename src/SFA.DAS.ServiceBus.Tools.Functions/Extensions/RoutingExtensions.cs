183c0a5393d2d4629b00b371753e1639e794100d
Jon Harris <jon.harris@fuitsu.com>
2024-09-27 12:38:43 +0100

    Routing fix

--- src/SFA.DAS.ServiceBus.Tools.Functions/Extensions/RoutingExtensions.cs
+++ src/SFA.DAS.ServiceBus.Tools.Functions/Extensions/RoutingExtensions.cs
@@ -10,8 +10,9 @@ public static class RoutingExtensions
     public static void AddRouting(this RoutingSettings routing)
     {
         routing.RouteToEndpoint(typeof(DraftExpireAccountFundsCommand), FinanceMessageHandlersEndpoint);
-        routing.RouteToEndpoint(typeof(DraftExpireAccountFundsCommand), FinanceMessageHandlersEndpoint);
+        routing.RouteToEndpoint(typeof(DraftExpireFundsCommand), FinanceMessageHandlersEndpoint);
         routing.RouteToEndpoint(typeof(ExpireAccountFundsCommand), FinanceMessageHandlersEndpoint);
+        routing.RouteToEndpoint(typeof(ExpireFundsCommand), FinanceMessageHandlersEndpoint);
         routing.RouteToEndpoint(typeof(ImportAccountLevyDeclarationsCommand), FinanceMessageHandlersEndpoint);
         routing.RouteToEndpoint(typeof(ImportPaymentsCommand), FinanceMessageHandlersEndpoint);
         routing.RouteToEndpoint(typeof(ProcessPeriodEndPaymentsCommand), FinanceMessageHandlersEndpoint);
