f7f67b3948b35de809565e50ada798fe67e13e35
Jon Harris <jon.harris@fuitsu.com>
2024-09-30 11:15:31 +0100

    Reinstating ConfigureFunctionsWorkerDefaults

--- src/SFA.DAS.ServiceBus.Tools.Functions/Program.cs
+++ src/SFA.DAS.ServiceBus.Tools.Functions/Program.cs
@@ -10,6 +10,7 @@ using SFA.DAS.ServiceBus.Tools.Functions.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWorkerDefaults()
     .ConfigureAppConfiguration(builder => builder.BuildDasConfiguration())
     .ConfigureNServiceBus()
     .ConfigureServices((context, services) =>
