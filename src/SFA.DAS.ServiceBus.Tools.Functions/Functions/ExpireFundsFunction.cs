427d6e4578a8404a46708aa8e8bc39d35874d00a
Jon Harris <jon.harris@fuitsu.com>
2024-09-30 09:45:58 +0100

    Logging

--- src/SFA.DAS.ServiceBus.Tools.Functions/Functions/ExpireFundsFunction.cs
+++ src/SFA.DAS.ServiceBus.Tools.Functions/Functions/ExpireFundsFunction.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.ServiceBus.Tools.Functions.Commands;
 using SFA.DAS.ServiceBus.Tools.Functions.Services;
 
 namespace SFA.DAS.ServiceBus.Tools.Functions.Functions;
 
-public class ExpireFundsFunction(IMessageProcessor messageProcessor)
+public class ExpireFundsFunction(IMessageProcessor messageProcessor, ILogger<ExpireFundsFunction> logger)
 {
     [Function("ExpireFunds")]
     public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post", "ExpireFunds")] HttpRequestData req, FunctionContext functionContext)
@@ -16,8 +18,9 @@ public class ExpireFundsFunction(IMessageProcessor messageProcessor)
         {
             await messageProcessor.SendCommand<ExpireFundsCommand>(functionContext);
         }
-        catch
+        catch(Exception exception)
         {
+            logger.LogError(exception, "Exception thrown in ExpireFundsFunction.");
             return req.CreateResponse(HttpStatusCode.BadRequest);
         }
 
